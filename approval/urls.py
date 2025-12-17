738b755f4d270b1746a5de467db86e47c4876616
Jeong hyun soo <nexussu@gmail.com>
2025-12-17 14:52:32 +0900

    Initial approval project

---
+++ approval/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('approvals.urls')),  # approvals/urls.py의 ''가 루트가 됨
+]
+
+# 개발용. 운영에서는 nginx가 media/static 처리
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
