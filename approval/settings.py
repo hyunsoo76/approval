b2c6a3d96e63466b89dc8b5c58a3231702bb4019
Jeong hyun soo <nexussu@gmail.com>
2026-01-14 14:35:17 +0900

    세팅에서얼라우드호스트냉무에서 넣음

--- approval/settings.py
+++ approval/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)d%ityva(jwym@u6j_hw(+n(h+og4#%)%-0z0s+m&+(1dqz=k5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.37.211.248", "localhost", "127.0.0.1"]
 
 
 # Application definition
