5fecad0c7cd027032d0370c2b9ca0a4535b2815b
Jeong hyun soo <nexussu@gmail.com>
2026-02-03 14:35:39 +0900

    chore: make FORCE_SCRIPT_NAME configurable by env

--- approval/settings.py
+++ approval/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-FORCE_SCRIPT_NAME = "/approval"
+FORCE_SCRIPT_NAME = os.environ.get("FORCE_SCRIPT_NAME", "")
 USE_X_FORWARDED_HOST = True
 LOGIN_URL = "/approval/admin/login/"
